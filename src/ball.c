1b03cfabe2fd5b69e0b6998654490be9137469de
devmadhav <sskzm6012@gmail.com>
2026-09-06 16:24:11 +0530

    fixed includes

---
+++ src/ball.c
@@ -0,0 +1,33 @@
+#include <raylib.h>
+#include "../include/config.h"
+#include "../include/ball.h"
+
+
+void update_and_draw_ball(
+    Ball* ball,
+    ScreenConfig screen,
+    int speed
+) {
+    switch (ball->state) {
+        case (BALL_NOT_SPAWNED):
+            break;
+
+        case (BALL_IDLE):
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                ball->state = BALL_MOVING;
+            }
+            break;
+            
+        case (BALL_MOVING):
+            if (ball->position.y < screen.height) {
+                ball->position.y += speed;
+            }
+            break;
+        }
+
+    DrawCircle(
+        (int)ball->position.x,
+        (int)ball->position.y,
+        10, BLACK
+    );
+}
